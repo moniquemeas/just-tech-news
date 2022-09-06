1fd7242b8c036e54207aaacf4549f1825ffe2c4f
Monique <monique.meas@gmail.com>
2022-09-06 00:34:28 -0400

    add logout session

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -64,11 +64,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-    res.status(500).json(err);
-    });
+    .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+      
+          res.json(dbUserData);
+        });
+      })
 });
 router.post('/login', (req, res) => {
 
@@ -138,6 +142,15 @@ router.delete('/:id', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+router.post('/logout', (req, res) => {
+if(req.session.loggedIn){
+    req.session.destroy(() => {
+        res.status(204).end();
+    });
+} else {
+    res.status(404).end();
+}
 });
 
 module.exports = router;
