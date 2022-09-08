e7cfd9b16825770be6d6ddbff63c92213b51347a
Monique <monique.meas@gmail.com>
2022-09-08 04:00:16 -0400

    add date and pluralize format

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
+// create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      port: 3306
     });
 
 module.exports = sequelize;
